46975fe55e61b6fddbfdd5136528f1d5311cf262
Phoebe Torchia <torchiaphoebe@gmail.com>
2023-10-27 13:33:54 -0400

    comments

--- example/main.dart
+++ example/main.dart
@@ -2,6 +2,7 @@ import 'package:cash_input_formatter/cash_input_formatter.dart';
 import 'package:flutter/material.dart';
 
 void main() {
+  // To run the example from the root directory, flutter run example
   runApp(const MyApp());
 }
 
@@ -9,6 +10,7 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
+    //Example App Route
     return MaterialApp(
       title: 'Cash Input Formatter Example',
       theme: ThemeData(
@@ -41,51 +43,65 @@ class _MyHomePageState extends State<MyHomePage> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              const Align(
-                alignment: Alignment.centerLeft,
-                child: Text("With symbol:"),
-              ),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Column(
-                  children: [
-                    const Text("Enter amount here"),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                        horizontal: 50,
-                      ),
-                      child: TextFormField(
-                        textAlign: TextAlign.center,
-                        inputFormatters: const [
-                          CashInputFormatter(),
-                        ],
-                      ),
+              // Shows widget with symbol
+              Column(
+                children: [
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text("With symbol:"),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: Column(
+                      children: [
+                        const Text("Enter amount here"),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(
+                            horizontal: 50,
+                          ),
+                          child: TextFormField(
+                            textAlign: TextAlign.center,
+                            // To use formatter simply add it as an input formatter.
+                            inputFormatters: const [
+                              CashInputFormatter(),
+                            ],
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
+                  ),
+                ],
               ),
+              // Padding between elements.
               const Padding(
                 padding: EdgeInsets.symmetric(vertical: 40.0),
               ),
-              const Align(
-                  alignment: Alignment.centerLeft,
-                  child: Text("Without symbol:")),
-              Padding(
-                padding: const EdgeInsets.symmetric(vertical: 16.0),
-                child: Column(
-                  children: [
-                    const Text("Enter amount here"),
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 50),
-                      child: TextFormField(
-                        textAlign: TextAlign.center,
-                        inputFormatters: const [
-                          CashInputFormatter(useSymbol: false)
-                        ],
-                      ),
-                    )
-                  ],
-                ),
+              // Shows widget without symbol
+              Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 16.0),
+                    child: Column(
+                      children: [
+                        const Align(
+                          alignment: Alignment.centerLeft,
+                          child: Text("Without symbol:"),
+                        ),
+                        const Text("Enter amount here"),
+                        Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 50),
+                          child: TextFormField(
+                            textAlign: TextAlign.center,
+                            inputFormatters: const [
+                              // To use formatter simply add it as an input formatter.
+                              CashInputFormatter(useSymbol: false)
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
               ),
             ],
           ),
